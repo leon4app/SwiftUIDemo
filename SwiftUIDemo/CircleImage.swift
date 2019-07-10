f01c26f5a3a150b638bab23716ea56e0645a6692
Leon Tse <xielh7@gmail.com>
2019-07-10 17:33:40 +0800

    + Add class CircleImage.

---
+++ SwiftUIDemo/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  SwiftUIDemo
+//
+//  Created by Leon on 2019/7/10.
+//  Copyright © 2019 Leon. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
