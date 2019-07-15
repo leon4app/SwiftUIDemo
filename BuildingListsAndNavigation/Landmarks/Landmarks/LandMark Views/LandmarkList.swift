7151b985019b62ba6a94094bb80cd95c3ef8c187
Leon Tse <xielh7@gmail.com>
2019-07-15 11:21:08 +0800

    + Add demo section Building Lists And Navigation.

---
+++ BuildingListsAndNavigation/Landmarks/Landmarks/LandMark Views/LandmarkList.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Leon on 2019/7/11.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList : View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkList_Previews : PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"].identified(by: \.self)) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+#endif
